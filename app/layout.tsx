5ceb83b80ecb95182d8e069b5287a78c245110e2
azaan-source <thefaheemiquasar@gmail.com>
2024-08-14 21:21:08 +0530

    fixed responsive design

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="max-w-10xl">
+        <main className="">
           <Navbar />
           {children}
         </main>
