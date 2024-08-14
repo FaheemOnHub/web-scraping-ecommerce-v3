f6909c734345a2424231a5e5edaccf67fafa901e
azaan-source <thefaheemiquasar@gmail.com>
2024-08-14 21:40:07 +0530

    fixed responsive design

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -13,6 +13,7 @@ import { NextResponse } from "next/server";
 
 export const maxDuration = 300;
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export async function GET() {
   try {
     connectToDB();
